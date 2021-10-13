5652bb0ff270b2c008cfe850906041f4d38dd15d
DylanDerouard <73218590+DylanDerouard@users.noreply.github.com>
2021-10-13 11:33:13 +0200

    inventory

--- pokemon.py
+++ pokemon.py
@@ -55,7 +55,7 @@ if __name__ == "__main__" :
     sum = 0
     test = {}
     result = {}
-    for i in range (1, 100):
+    for i in range (1, 10):
         test[i] = Pokemon(i)
         print(test[i].pokemon.name)
         sum += test[i].spawnrate
@@ -79,27 +79,131 @@ Pokeball =[
  
     {
         "name" : "Pokeball",
-        "percent" : 30
+        "percent" : 30,
+        "total" : 80
+
     },
      {
         "name" : "Superball",
-        "percent" : 50
+        "percent" : 50,
+        "total" : 50
     },
      {
         "name" : "Hyperball",
-        "percent" : 70
+        "percent" : 70,
+        "total" : 30
     },
     {   "name" : "Masterball",
-        "percent" : 100
+        "percent" : 100,
+        "total" : 1
+    },
+    {
+        "name" : "PokeDollars",
+        "total" : 5000
     }
 ]
 
-def pokemonsavage (name):
-    print("                                  ")
-    print("                                  ")
-    print("  Un ",name," sauvage est apparu  ")
-    print("                                  ")
-    print(" 1-Combattre   -2Attraper   3-Fuir")
-    print("                                  ")
-    print("                                  ")
+
+def lePokemonSpawn(name): 
+    print("****************************************")
+    print("                                        ")
+    print("    Un ",name," sauvage est apparu !    ")
+    print("                                        ")
+    print("    1- Capturer  2- Fuir  3- Attaquer   ")
+    print("                                        ")
+    print("****************************************")
+
+    while True:
+            input1 = int(input())
+            if  input1 == 1:
+                capture(name)
+                break
+            elif input1 == 2:
+                print("***********************************")
+                print("                                  -")
+                print("     Vous avez pris la fuite !    -")
+                print("                                  -")
+                print("***********************************")
+                break
+
+def capture(name):
+      
+        print("***********************************")
+        print("-                                 -")
+        print("-   1- Pokeball    2- Superball   -")
+        print("-                                 -")
+        print("-   3- Hyperball   4- Masterball  -")
+        print("-                                 -")
+        print("***********************************")
+
+        while True:
+            input3 = int(input())
+            poke = Pokemon(1)
+            a = random.randint(1, 100)
+            if input3 == 4:
+                Pokeball[input3-1]["total"]-=1
+                print(Pokeball)
+                print("***********************************")
+                print("-                                 -")
+                print("-    Vous avez capturé",name,"!   -")
+                print("-                                 -")
+                print("***********************************")
+                break
+            if (Pokeball[input3-1]['percent']/(1+(poke.resistancerate/100))) >= a:
+                Pokeball[input3-1]["total"]-=1
+                print(Pokeball)
+                print("***********************************")
+                print("-                                 -")
+                print("-    Vous avez capturé",name,"!   -")
+                print("-                                 -")
+                print("***********************************")
+                break
+            else:
+                print("Raté ! ")
+                Pokeball[input3-1]["total"]-=1
+                print(Pokeball)
+                capture(name)
+            
+            
+class Pokemon():
+    max = 0
+
+    def __init__(self, id):
+        self.timeStart = time.time_ns()           
+        self.pokemon = pypokedex.get(dex = id)
+        self.spawnrate = random.randint(0,100)
+        self.resistancerate = random.randint(0,50)
+        self.startSpawn = self.__class__.max
+        self.__class__.max += self.spawnrate
+        self.endSpawn = self.__class__.max
+
+    def __repr__(self):
+        return self.pokemon.name
+
+
+if __name__ == "__main__" :
+    sum = 0
+    test = {}
+    result = {}
+    for i in range (1, 10):
+        test[i] = Pokemon(i)
+        print(test[i].pokemon.name)
+        sum += test[i].spawnrate
+        result[test[i].pokemon.name] = 0
+    
+    for i in test:
+         print("le pokémon : ", test[i].pokemon.name, "  a une chance de spawn de : ", "{:.2f}".format(test[i].spawnrate/(test[i].max/10)), "%")
+
+    for i in range(1):
+        spawn = random.randint(1, test[1].max)
+        for i in test:
+            if spawn >= test[i].startSpawn and spawn < test[i].endSpawn:
+                # print("le pokemon est spawn: ", test[i].pokemon.name, "   et son taux de spawn est:  ", test[i].spawnrate, "%")
+                result[test[i].pokemon.name] += 1
+                break
+        for i in result:
+            if result[i] != 0:
+                #print("pokémon: ", i, "  quantité:  ", result[i], "pourcentage: ", "{:.2f}".format(result[i]/100))
+                lePokemonSpawn(i)
+
 
